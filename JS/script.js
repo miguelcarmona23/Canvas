2ff057de17e7835bebaf981feab90cf2f6412ecf
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-06 11:08:07 -0500

    Fixed missing x in ctx

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,6 @@ const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ct.strokeStyle = '#BADA55';
+ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round'
 ctx.lineCap = 'round';
