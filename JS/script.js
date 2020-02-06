35d4d6058ca0527cc783abd55ff924ac10c48714
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-06 12:46:35 -0500

    Added globalCompositeOperation

--- JS/script.js
+++ JS/script.js
@@ -7,6 +7,7 @@ ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 100;
+ctx.globalCompositeOperation = 'multiply';
 
 let isDrawing = false;
 let lastX = 0;
